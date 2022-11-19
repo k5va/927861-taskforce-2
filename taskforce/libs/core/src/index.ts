9cbc90b23432e506fa9f4832864b6ea324bb13bf
k5va <k5va@yandex.ru>
2022-11-19 17:38:33 +0500

    feat(lib): added CRUDRepository interface

--- taskforce/libs/core/src/index.ts
+++ taskforce/libs/core/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/fill-object';
+export * from './lib/crud-repository.interface';
--- taskforce/libs/shared-types/src/lib/user.interface.ts
+++ taskforce/libs/shared-types/src/lib/user.interface.ts
@@ -1,12 +1,12 @@
 import { UserRole } from './user-role.enum';
 
 export interface User {
-  _id: string,
-  name: string,
-  email: string,
-  city: string,
-  passwordHash: string,
-  role: UserRole,
-  avatar: string,
-  birthDate: Date,
+  _id: string;
+  name: string;
+  email: string;
+  city: string;
+  passwordHash: string;
+  role: UserRole;
+  avatar: string;
+  birthDate: Date;
 }
