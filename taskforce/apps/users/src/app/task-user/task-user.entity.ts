d624a424399cdc1726d06f4eacfc6a8276010044
k5va <k5va@yandex.ru>
2022-11-19 17:55:12 +0500

    feat(users): added TaskUserEntity

---
+++ taskforce/apps/users/src/app/task-user/task-user.entity.ts
@@ -0,0 +1,40 @@
+import { User, UserRole } from '@taskforce/shared-types';
+import { genSalt, compare, hash } from 'bcrypt';
+import { SALT_ROUNDS } from './task-user.const';
+
+export class TaskUserEntity implements User {
+  public _id: string;
+  public name: string;
+  public email: string;
+  public city: string;
+  public passwordHash: string;
+  public role: UserRole;
+  public avatar: string;
+  public birthDate: Date;
+
+  constructor(data: User) {
+    this._id = data._id;
+    this.name = data.name;
+    this.email = data.email;
+    this.city = data.city;
+    this.passwordHash = data.passwordHash;
+    this.role = data.role;
+    this.avatar = data.avatar;
+    this.birthDate = data.birthDate;
+  }
+
+  public toObject(): User {
+    return {...this};
+  }
+
+  public async setPassword(password: string): Promise<TaskUserEntity> {
+    const salt = await genSalt(SALT_ROUNDS);
+    this.passwordHash = await hash(password, salt);
+    return this;
+  }
+
+  public async comparePassword(password: string): Promise<boolean> {
+    return compare(password, this.passwordHash);
+  }
+
+}
