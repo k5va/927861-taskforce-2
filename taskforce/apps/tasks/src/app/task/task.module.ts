0df35f433a637b964a818fb4eb569a273436af88
k5va <k5va@yandex.ru>
2022-11-21 21:14:28 +0500

    feat(tasks): imported comment service in task module

--- taskforce/apps/tasks/src/app/task/task.module.ts
+++ taskforce/apps/tasks/src/app/task/task.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CommentService } from '../comment/comment.service';
 import { TaskMemoryRepository } from './repository/task-memory.repository';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
 
 @Module({
+  imports: [CommentService],
   controllers: [TaskController],
   providers: [TaskMemoryRepository, TaskService]
 })
