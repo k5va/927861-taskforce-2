ebc7bb261bf72fc3bdca486a9f3c81e818987bd3
k5va <k5va@yandex.ru>
2022-12-12 20:34:30 +0500

    feat(tasks): added category, comment dto validation

--- taskforce/apps/tasks/src/app/category/dto/create-category.dto.ts
+++ taskforce/apps/tasks/src/app/category/dto/create-category.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class CreateCategoryDto {
   @ApiProperty({
@@ -6,5 +7,6 @@ export class CreateCategoryDto {
     required: true,
     example: 'cleaning',
   })
+  @IsString()
   public name: string;
 }
--- taskforce/apps/tasks/src/app/comment/comment.const.ts
+++ taskforce/apps/tasks/src/app/comment/comment.const.ts
@@ -1,1 +1,4 @@
 export const COMMENT_NOT_FOUND_ERROR = 'comment not found';
+
+export const COMMENT_MIN_LENGTH = 10;
+export const COMMENT_MAX_LENGTH = 300;
