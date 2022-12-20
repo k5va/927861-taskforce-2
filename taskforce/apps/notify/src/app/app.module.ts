ba2a2e7f54b089e050b19e05bbff8ce670c7bef0
k5va <k5va@yandex.ru>
2022-12-20 11:20:44 +0500

    feat(notify): configured nodemailer

--- taskforce/apps/notify/src/app/app.module.ts
+++ taskforce/apps/notify/src/app/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule } from '@nestjs/config';
+import { SenderModule } from './sender/sender.module';
+import { ENV_FILE_PATH } from './app.const';
+import { getMailerOptions } from '../config/get-mailer-options';
+import { mailConfig } from '../config/mail.config';
+import envSchema from './env.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+      load: [mailConfig],
+      validationSchema: envSchema,
+    }),
+    MailerModule.forRootAsync(getMailerOptions()),
+    SenderModule,
+  ],
   controllers: [],
   providers: [],
 })
