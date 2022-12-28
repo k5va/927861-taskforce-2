53fe41c91b0216616f1379459126ac9274a10654
k5va <k5va@yandex.ru>
2022-12-28 17:03:46 +0500

    feat(notify): Adds mongodb config

--- taskforce/apps/notify/src/app/app.module.ts
+++ taskforce/apps/notify/src/app/app.module.ts
@@ -3,9 +3,14 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule } from '@nestjs/config';
 import { SenderModule } from './sender/sender.module';
 import { ENV_FILE_PATH } from './app.const';
-import { getMailerOptions } from '../config/get-mailer-options';
-import { mailConfig } from '../config/mail.config';
+import {
+  getMailerOptions,
+  getMongoDbOptions,
+  mailConfig,
+  mongoDbConfig,
+} from '../config';
 import envSchema from './env.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -13,10 +18,11 @@ import envSchema from './env.schema';
       cache: true,
       isGlobal: true,
       envFilePath: ENV_FILE_PATH,
-      load: [mailConfig],
+      load: [mailConfig, mongoDbConfig],
       validationSchema: envSchema,
     }),
     MailerModule.forRootAsync(getMailerOptions()),
+    MongooseModule.forRootAsync(getMongoDbOptions()),
     SenderModule,
   ],
   controllers: [],
--- taskforce/apps/notify/src/app/env.schema.ts
+++ taskforce/apps/notify/src/app/env.schema.ts
@@ -5,4 +5,10 @@ export default Joi.object({
   MAIL_HOST: Joi.string().hostname().required(),
   MAIL_PORT: Joi.number().port().required(),
   MAIL_FROM: Joi.string().required(),
+  MONGO_DB: Joi.string().required(),
+  MONGO_HOST: Joi.string().hostname().required(),
+  MONGO_PORT: Joi.number().port().required(),
+  MONGO_USER: Joi.string().required(),
+  MONGO_PASSWORD: Joi.string().required(),
+  MONGO_AUTH_BASE: Joi.string().required(),
 });
